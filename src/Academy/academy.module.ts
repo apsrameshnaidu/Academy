9e3556b904a01665f8c758ef1ab902c02dca949e
S Ramesh Naidu <54275177+apsrameshnaidu@users.noreply.github.com>
2019-09-10 17:13:20 +0530

    Delete academy.module.ts

--- src/Academy/academy.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AcademyService } from './academy.service';
-import { AcademyController } from './academy.controller';
- 
-@Module({
-  providers: [AcademyService],
-  controllers: [AcademyController],
-})
-export class AcademyModule {}
